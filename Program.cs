d9026d663451398ee00c370ae6ea08980eea8a39
Timur <t.mollaev75@yandex.ru>
2023-03-03 22:44:29 +0300

    homework1

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// Console.WriteLine("Input first namber");
+// int nam1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second namber");
+// int nam2 = Convert.ToInt32(Console.ReadLine());
+// if (nam1>nam2)
+//     Console.WriteLine($"Max namber -> {nam1}");
+// else
+//     Console.WriteLine($"Max namber -> {nam2}");      
+
+
+
+// Console.WriteLine("Input first namber");
+// int nam1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second namber");
+// int nam2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third namber");
+// int nam3 = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"{nam1}, {nam2}, {nam3} ");
+// if (nam1>=nam2)
+// if (nam1>=nam3)
+//     Console.WriteLine($"Max namber -> {nam1}");
+// if (nam2>=nam1)
+// if (nam2>=nam3)
+//     Console.WriteLine($"Max namber -> {nam2}");
+// if (nam3>=nam1)
+// if (nam3>=nam2)
+//     Console.WriteLine($"Max namber -> {nam3}");
+
+
+
+// Console.WriteLine("Input namber");
+// int nam = Convert.ToInt32(Console.ReadLine());
+// if(nam%2==0)
+//     Console.WriteLine($"{nam} -> Да");
+// else
+//     Console.WriteLine($"{nam} -> Нет");  
+
+
+
+// Console.WriteLine("Input namber");
+// int nam = Convert.ToInt32(Console.ReadLine());
+// int current = 1;
+// Console.Write($"{nam} ->");
+// while(current <=nam)
+// {
+//   if(current%2==0)
+//     Console.Write($" {current + " " }");
+//     current++;
+// }    
+
+Program
