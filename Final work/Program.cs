dd889b5d209f52015093f9646f69a03b30541fce
Timur <t.mollaev75@yandex.ru>
2023-05-14 19:03:21 +0300

    обьявили массив

---
+++ Final work/Program.cs
@@ -0,0 +1,8 @@
+﻿
+string[] array = new string[] {"geek", "brains", "it", "!"};
+Console.Write( array[0]+ " " );
+Console.Write( array[1]+ " " );
+Console.Write( array[2] );
+Console.Write( array[3]+ " " );
+Console.Write ("=>"+ " ");
+
